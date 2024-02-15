31589da3439b0b72211ac71264d1f05c0d91165c
Karina Chaires <kdchaires@gmail.com>
2024-02-14 23:52:18 -0600

    Updated to GHC 9.6.3

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # Weekly Assignments 6
 
-### To be submitted: Friday, 11 September 2020, 12:30 MNG
 
 Note that some tasks may deliberately ask you to look at concepts or libraries
 that we have not yet discussed in detail. But if you are in doubt about the
--- doctest/doctests.hs
+++ doctest/doctests.hs
@@ -1,14 +1,17 @@
 module Main where
 
-import Build_doctests            (flags, pkgs, module_sources)
-import Data.Foldable             (traverse_)
-import System.Environment.Compat (unsetEnv)
+-- import Build_doctests            (flags, pkgs, module_sources)
+-- import Data.Foldable             (traverse_)
+-- import System.Environment.Compat (unsetEnv)
 import Test.DocTest              (doctest)
 
 main :: IO ()
-main = do
+main = doctest ["src"]
+
+{- main = do
     traverse_ putStrLn args
     unsetEnv "GHC_ENVIRONMENT"
     doctest args
   where
     args = flags ++ pkgs ++ module_sources
+-}
